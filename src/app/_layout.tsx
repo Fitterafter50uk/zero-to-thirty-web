89fd538e8950b5c6108a992f619b389c8acf48fe
Craig Murray <craig.potterhill@outlook.com>
2026-08-15 19:35:51 +0100

    Fix web render issue

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from 'expo-router';
-import { Head } from 'expo-router/head';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
@@ -13,10 +12,7 @@ export default function RootLayout() {
 
   return (
     <>
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#FF8C00" />
-      </Head>
+      
 
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <AnimatedSplashOverlay />
